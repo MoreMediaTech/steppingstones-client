960a34f8ef22eeaee90e2f57ecd50aa02d3e10bb
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-06 14:43:01 +0100

    steppingstones-client: modified authService

--- lib/axiosDefaultConfig.ts
+++ lib/axiosDefaultConfig.ts
@@ -4,7 +4,6 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
-    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
   },
   withCredentials: true,
