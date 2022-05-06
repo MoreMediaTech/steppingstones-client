46e732cb969f1ea6285a5f9af218296f93328dc7
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-06 14:23:03 +0100

    steppingstones-client: created _error.tsx to catch all errors

--- features/auth/authService.ts
+++ features/auth/authService.ts
@@ -24,7 +24,6 @@ const register = async (data: Partial<CurrentUser>) => {
 const login = async (data: Partial<CurrentUser>) => {
   const response = await axios.post(`${API_URL}auth/login`, data, {
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: false,
   })
 
   if (response.data) {
