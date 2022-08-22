3731e629012a2bb5c34a747abfb5995c3b489c63
Adewoyin Oladipupo-Usoro <aolausoro@gmail.com>
2022-08-22 11:04:06 +0000

    steppingstones-client: modified section page

--- pages/admin/editor-portal/county-portal/[county]/section/index.tsx
+++ pages/admin/editor-portal/county-portal/[county]/section/index.tsx
@@ -122,10 +122,8 @@ const Section = ({
                         <div className="h-full rounded bg-white p-2 shadow-md md:col-span-2">
                           <Map location={`${county}, UK`} />
                         </div>
-                        <div className="h-full w-full md:w-3/4">
-                          <div className="flex flex-col">
-                            <div className="w-full space-y-4 py-8">
-                              <div className="grid grid-cols-2 gap-y-4 gap-x-4">
+                        <div className="h-full w-full">
+                              <div className="grid grid-cols-2 gap-4">
                                 {sectionData?.subsections?.map(
                                   (section: SubSectionProps) => (
                                     <PortalButton
@@ -149,8 +147,6 @@ const Section = ({
                                   )
                                 )}
                               </div>
-                            </div>
-                          </div>
                         </div>
                       </div>
                     )}
