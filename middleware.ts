183d7b6d561506aef7dc01c0073866a186ecee0c
A Oladipupo-Usoro <aolausoro@gmail.com>
2023-01-22 16:40:22 +0000

    steppingstones-client: resolved Type error: Type 'RequestCookie' is not comparable to type 'string' in middleware.ts.

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import type { NextRequest } from 'next/dist/server/web/spec-extension/request'
 import { NEXT_URL } from './config'
 
 export function middleware(request: NextRequest) {
-    const cookies = request.cookies.get('ss_refresh_token') as string
-  if (!cookies  && request.nextUrl.pathname.startsWith('/admin')) {
+    const cookies = request.cookies.get('ss_refresh_token')
+  if ((!cookies  || cookies === undefined )&& request.nextUrl.pathname.startsWith('/admin') ) {
     return NextResponse.redirect(`${NEXT_URL}/auth/login`)
   }
 
