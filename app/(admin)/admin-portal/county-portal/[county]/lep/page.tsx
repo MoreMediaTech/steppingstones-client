41344e669ef150fcf8a5979472f0dd82eedb618b
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-06-07 21:37:51 +0100

    resolved Type Error.

--- app/(admin)/admin-portal/county-portal/[county]/lep/page.tsx
+++ app/(admin)/admin-portal/county-portal/[county]/lep/page.tsx
@@ -22,7 +22,6 @@ export default function Page({
         <LEP
           id={searchParams.countyId}
           county={searchParams.county}
-          countyId={searchParams.countyId}
         />
       </section>
     </section>
