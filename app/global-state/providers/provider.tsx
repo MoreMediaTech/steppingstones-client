7dad0ca5b2befb14d018c0d239ebbd9c81397059
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-06-18 17:10:23 +0100

    resolved Type error: Cannot find module '@mui/material' or its corresponding type declarations.

--- app/global-state/providers/provider.tsx
+++ app/global-state/providers/provider.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 import RTKProvider from './rtk-providers'
 import NextThemeProvider from './nexttheme-provider'
-import MaterialThemeProvider from './material-provider'
 import GlobalMantineProvider from './mantine-provider'
 
 export default function Provider({ children }: { children: React.ReactNode }) {
