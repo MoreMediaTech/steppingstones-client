e0c701bfe021a58f4578558473e9261bd2005f08
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-07-08 22:48:29 +0100

    Type error: Module '@lib/types' has no exported member 'CurrentUser'.

--- app/(admin)/admin-portal/page.tsx
+++ app/(admin)/admin-portal/page.tsx
@@ -1,26 +1,13 @@
-'use client'
-import { Loader } from '@components/mantine-components'
-
-import { CurrentUser } from '@lib/types'
-import { useGetUserQuery } from 'app/global-state/features/user/usersApiSlice'
-import PortalHeader from 'app/components/PortalHeader'
-import PortalSection from 'app/components/PortalHeader/PortalSection'
+import Loader from '@components/Loader'
 import { RenderInsights } from './Insights'
+import { Suspense } from 'react'
 
 export default function AdminPortal() {
-  const { data: user, isLoading } = useGetUserQuery()
   return (
     <>
-
-      {isLoading ? (
-        <div className="flex h-[700px] items-center justify-center">
-          <Loader size="xl" variant="bars" />
-        </div>
-      ) : (
-        <>
+      <Suspense fallback={<Loader />}>
         <RenderInsights />
-        </>
-      )}
+      </Suspense>
     </>
   )
 }
