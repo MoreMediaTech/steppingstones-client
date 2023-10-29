079286115329fd223d272cef2de79827dc15823d
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-10-29 21:39:39 +0000

    Create support page, controller and api-slice.

---
+++ app/(admin)/admin-portal/support/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Support } from './support'
+
+export default function SupportPage() {
+  return (
+    <section className='w-full'>
+        <Support />
+    </section>
+  )
+}
