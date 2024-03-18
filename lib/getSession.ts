f770721b74f5172403f449066fef9657e6eaae36
A Oladipupo-Usoro <aolausoro@gmail.com>
2024-03-18 14:30:35 +0000

    Refactor getSession function to be asynchronous

--- lib/getSession.ts
+++ lib/getSession.ts
@@ -1,7 +1,7 @@
 "use server";
 import { cookies } from "next/headers";
 
-export function getSession() {
+export async function getSession() {
   const session = cookies().get("connect.sid")?.value;
   if (!session) return null;
   return session;
