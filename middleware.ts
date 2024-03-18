d5132db6f038185c9d33d4cebbd6d69f7f4028ff
A Oladipupo-Usoro <aolausoro@gmail.com>
2024-03-18 12:47:54 +0000

    Update Node.js version and comment out middleware code

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,19 @@ import { NEXT_URL } from "./config";
 import { getSession } from "@lib/getSession";
 
 export function middleware(request: NextRequest) {
-  const cookies = getSession();
-  if (
-    !cookies &&
-    request.nextUrl.pathname.startsWith("${NEXT_URL}/admin-portal")
-  ) {
-    return NextResponse.redirect(`${NEXT_URL}/auth/login`);
-  }
-  if (
-    cookies &&
-    request.nextUrl.pathname.startsWith("${NEXT_URL}/auth/login")
-  ) {
-    return NextResponse.redirect(`${NEXT_URL}/admin-portal`);
-  }
+  // const cookies = getSession();
+  // if (
+  //   !cookies &&
+  //   request.nextUrl.pathname.startsWith("${NEXT_URL}/admin-portal")
+  // ) {
+  //   return NextResponse.redirect(`${NEXT_URL}/auth/login`);
+  // }
+  // if (
+  //   cookies &&
+  //   request.nextUrl.pathname.startsWith("${NEXT_URL}/auth/login")
+  // ) {
+  //   return NextResponse.redirect(`${NEXT_URL}/admin-portal`);
+  // }
 }
 
 // See "Matching Paths" below to learn more
