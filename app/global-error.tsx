8e2ac51950b37f39e8100bac32e3a4712d7a4da4
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2024-03-20 19:16:01 +0000

    Add useCallback hook to global-error component

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@components/ui/button";
 import useHasMounted from "@hooks/useHasMounted";
